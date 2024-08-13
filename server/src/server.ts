d707eb75655ea3bd8601b85993193e85ab410f10
diyaayay <diya.solanki.31@gmail.com>
2024-08-13 15:57:03 +0530

    Sets up server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,170 @@
 
-let kanchu : String = "haaye ram";
-let gadhu = "dklajlkasajdslkajdsdlkj";
-interface myGadhu {
-    name: String
+import {
+	createConnection,
+	TextDocuments,
+	Diagnostic,
+	DiagnosticSeverity,
+	ProposedFeatures,
+	InitializeParams,
+	DidChangeConfigurationNotification,
+	CompletionItem,
+	CompletionItemKind,
+	TextDocumentPositionParams,
+	TextDocumentSyncKind,
+	InitializeResult,
+	DocumentDiagnosticReportKind,
+	type DocumentDiagnosticReport
+} from 'vscode-languageserver/node';
+
+import {
+	TextDocument
+} from 'vscode-languageserver-textdocument';
+
+
+const connection = createConnection(ProposedFeatures.all);
+
+
+const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+
+
+let hasConfigurationCapability = false;
+let hasWorkspaceFolderCapability = false;
+let hasDiagnosticRelatedInformationCapability = false;
+
+connection.onInitialize((params: InitializeParams) => {
+	const capabilities = params.capabilities;
+
+	hasConfigurationCapability = !!(
+		capabilities.workspace && !!capabilities.workspace.configuration
+	);
+	hasWorkspaceFolderCapability = !!(
+		capabilities.workspace && !!capabilities.workspace.workspaceFolders
+	);
+	hasDiagnosticRelatedInformationCapability = !!(
+		capabilities.textDocument &&
+		capabilities.textDocument.publishDiagnostics &&
+		capabilities.textDocument.publishDiagnostics.relatedInformation
+	);
+
+	const result: InitializeResult = {
+		capabilities: {
+			textDocumentSync: TextDocumentSyncKind.Incremental,
+			completionProvider: {
+				resolveProvider: true,
+				triggerCharacters: [ '.' ]
+			},
+			diagnosticProvider: {
+				interFileDependencies: false,
+				workspaceDiagnostics: false
+			},
+			hoverProvider: true,
+			definitionProvider : true,
+			codeLensProvider : {
+				resolveProvider: true
+			},
+			referencesProvider: true,
+			renameProvider: true
+		}
+	};
+	if (hasWorkspaceFolderCapability) {
+		result.capabilities.workspace = {
+			workspaceFolders: {
+				supported: true
+			}
+		};
+	}
+	return result;
+});
+
+connection.onInitialized(() => {
+	if (hasConfigurationCapability) {
+		connection.client.register(DidChangeConfigurationNotification.type, undefined);
+	}
+	if (hasWorkspaceFolderCapability) {
+		connection.workspace.onDidChangeWorkspaceFolders(_event => {
+			connection.console.log('Workspace folder change event received.');
+		});
+	}
+});
+
+
+interface ExampleSettings {
+	sketchFile: number;
 }
+
+const defaultSettings: ExampleSettings = { sketchFile: 1 };
+let globalSettings: ExampleSettings = defaultSettings;
+
+
+const documentSettings: Map<string, Thenable<ExampleSettings>> = new Map();
+
+connection.onDidChangeConfiguration(change => {
+	if (hasConfigurationCapability) {
+		
+		documentSettings.clear();
+	} else {
+		globalSettings = <ExampleSettings>(
+			(change.settings.languageServerExample || defaultSettings)
+		);
+	}
+
+	connection.languages.diagnostics.refresh();
+});
+
+function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
+	if (!hasConfigurationCapability) {
+		return Promise.resolve(globalSettings);
+	}
+	let result = documentSettings.get(resource);
+	if (!result) {
+		result = connection.workspace.getConfiguration({
+			scopeUri: resource,
+			section: 'languageServerExample'
+		});
+		documentSettings.set(resource, result);
+	}
+	return result;
+}
+
+documents.onDidClose(e => {
+	documentSettings.delete(e.document.uri);
+});
+
+export let latestChangesInTextDoc: TextDocument
+
+//ToDo setup Log
+documents.onDidChangeContent(change => {
+	//validate sketch
+});
+
+ 
+
+connection.onDidChangeWatchedFiles(_change => {
+	
+	connection.console.log('We received a file change event');
+});
+
+
+//perform auto completion
+connection.onCompletion(
+	(_textDocumentParams: CompletionParams): CompletionItem[] => {
+		return completion.decideCompletionMethods(_textDocumentParams, latestChangesInTextDoc)
+	}
+);
+
+connection.onCompletionResolve(
+	(item: CompletionItem): CompletionItem => {
+	// item.detail:"",
+	// item.documentation:""
+	// return item;
+	
+	return {};
+	}
+);
+
+//ToDo: On hover
+
+documents.listen(connection);
+
+connection.listen();
