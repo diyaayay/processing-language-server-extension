1d99b51cb440817fecfbc66969725a3c215dde83
diyaayay <diya.solanki.31@gmail.com>
2024-08-22 16:08:26 +0530

    Pde specific settings in client

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,5 +1,6 @@
 import * as path from 'path';
 import { workspace, ExtensionContext } from 'vscode';
+import { SketchRunner } from "./sketchRunner"; // TBD
 
 import {
 	LanguageClient,
@@ -15,28 +16,41 @@ export function activate(context: ExtensionContext) {
 		path.join('server', 'out', 'server.js')
 	);
 
+	let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+
 	const serverOptions: ServerOptions = {
 		run: { module: serverModule, transport: TransportKind.ipc },
 		debug: {
 			module: serverModule,
 			transport: TransportKind.ipc,
+			options: debugOptions
 		}
 	};
 
 	const clientOptions: LanguageClientOptions = {
 		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
 		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+			fileEvents: workspace.createFileSystemWatcher('**/*')
 		}
 	};
 
 	client = new LanguageClient(
-		'languageServerExample',
-		'Language Server Example',
+		'processingLanguageServer',
+		'Processing Language Server',
 		serverOptions,
 		clientOptions
 	);
+
+	//processign specific paths
+let clientPath = context.asAbsolutePath(path.join('client'));
+let serverPath = context.asAbsolutePath(path.join('server'));
+let jrePath = context.asAbsolutePath(path.join('jre', 'bin'));
+
+
+let serverCompilePath = path.join(serverPath, 'out', 'compile')
+let clientSketchPath = path.join(clientPath, 'out', 'class')
+
+//Setup instance from sketchRunner(TODO)
 
 	client.start();
 }
