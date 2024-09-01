7c91e7a99172740fda46ff796ea35af1a713cb55
diyaayay <diya.solanki.31@gmail.com>
2024-09-01 16:11:54 +0530

    java specific grammar

---
+++ server/src/grammar/javaSpecific.ts
@@ -0,0 +1,71 @@
+// Java Top level keywords
+export const TOP_LEVEL_KEYWORDS: string[] = [
+	"package",
+	"import",
+	"public",
+	"private",
+	"protected",
+	"abstract",
+	"class",
+	"interface",
+	"extends",
+	"implements"
+];
+
+
+// Keywords inside class body
+export const CLASS_BODY_KEYWORDS: string[] = [
+	"public",
+	"protected",
+	"private",
+	"static",
+	"final",
+	"native",
+	"synchronized",
+	"abstract",
+	"default",
+	"class",
+	"interface",
+	"void",
+	"boolean",
+	"int",
+	"long",
+	"float",
+	"double"
+];
+
+//inside method body
+export const METHOD_BODY_KEYWORDS: string[] = [
+	"new",
+	"assert",
+	"try",
+	"catch",
+	"finally",
+	"throw",
+	"return",
+	"break",
+	"case",
+	"continue",
+	"default",
+	"do",
+	"while",
+	"for",
+	"switch",
+	"if",
+	"else",
+	"instanceof",
+	"var",
+	"final",
+	"class",
+	"void",
+	"boolean",
+	"int",
+	"long",
+	"float",
+	"double",
+	"boolean",
+	"char",
+	"byte",
+	"short",
+	"string"
+];
