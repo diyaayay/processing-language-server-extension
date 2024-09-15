526bdfca52aff8da0c48debb8216e98aa4bbfc17
diyaayay <diya.solanki.31@gmail.com>
2024-09-16 05:07:43 +0530

    added support for go-to definition

--- server/src/definition.ts
+++ server/src/definition.ts
@@ -11,75 +11,75 @@ import { getCharacterOffset,
 import { TOP_LEVEL_KEYWORDS } from './grammar/terms/javaSpecific';
 import { Definition } from "vscode-languageserver";
 
-let foundDeclaration: [string, string, number, number][] = new Array();
+let foundDeclaration: [string,string,number,number][] = new Array();
 let _foundDeclarationCount = 0;
 
-export function scheduleLookUpDefinition(receivedUri: string, lineNumber: number, charNumber: number): Definition | null {
+export function scheduleLookUpDefinition(receivedUri: string, lineNumber: number, charNumber: number): Definition | null  {
 	let currentContent = getTabContent(receivedUri);
 	if (!currentContent) {
 		return null;
 	}
-	let splitDefine = currentContent.split(`\n`);
+	let splitDefine = currentContent.split("\n");
 	let currentLine = splitDefine[lineNumber];
-	let cuurentDefineMap = lineMap(currentLine);
+	let currentDefineMap = lineMap(currentLine);
 	let adjustOffset = getLineOffset();
 	let tokenArray = getTokenArray();
 
-	tokenArray.forEach(function(token) {
-		if(token[1] instanceof ClassDeclarationContext) {
-			if (!(TOP_LEVEL_KEYWORDS.indexOf(token[0].text) > -1)) {
-				foundDeclaration[_foundDeclarationCount] = [`class`, token[0].text, token[0].payload._line, token[0].payload._charPositionInline];
-				_foundDeclarationCount += 1;
+	tokenArray.forEach(function(token){
+		if(token[1] instanceof ClassDeclarationContext){
+			if(!(TOP_LEVEL_KEYWORDS.indexOf(token[0].text) > -1)){
+				foundDeclaration[_foundDeclarationCount] = [`class`, token[0].text, token[0].payload._line, token[0].payload._charPositionInLine];
+				_foundDeclarationCount +=1;
 			}
-		} else if(token[1] instanceof MethodDeclarationContext) {
+		} else if(token[1] instanceof VariableDeclaratorIdContext){
 			foundDeclaration[_foundDeclarationCount] = [`var`, token[0].text, token[0].payload._line, token[0].payload._charPositionInLine];
-		} else if (token[1] instanceof MethodDeclarationContext) {
-			foundDeclaration[_foundDeclarationCount] = [`method`, token[0].text, token[0].payload._line, token[0].payload._charPositionInLine]
+			_foundDeclarationCount +=1;
+		} else if(token[1] instanceof MethodDeclarationContext){
+			foundDeclaration[_foundDeclarationCount] = [`method`, token[0].text, token[0].payload._line, token[0].payload._charPositionInLine];
 			_foundDeclarationCount +=1;
 		}
-	});
+	})
 
 	let finalDefinition: Definition | null = null;
-	cuurentDefineMap.forEach(function(word) {
-		if((word[1] <= charNumber) && (charNumber <= word[2])) {
-			foundDeclaration.forEach(function(declarationName) {
-				if(word[0] == declarationName[1]) {
+	currentDefineMap.forEach(function(word){
+		if((word[1] <= charNumber) && (charNumber <= word[2])){
+			foundDeclaration.forEach(function(declarationName){
+				if(word[0] == declarationName[1]){
 
-					let lineNumberJavaFile = declarationName[2] - adjustOffset;
+					let lineNumberJavaFile = declarationName[2]-adjustOffset;
 					let diffLine : number = 0;
 					let docUri : string = '';
 					let transformMap = getTransformationMap();
 					if (transformMap.get(lineNumberJavaFile)) {
 						diffLine = transformMap.get(lineNumberJavaFile)!.lineNumber;
 						let docName =  transformMap.get(lineNumberJavaFile)!.fileName;
-						docUri = getInfo().uri+docName;
+						docUri = getInfo().uri + docName;
 					}
+
 					let charOffset = getCharacterOffset(lineNumberJavaFile, declarationName[2]);
 
 					finalDefinition = {
 						uri: docUri,
-						range: {
-							start : {
-								line : diffLine-1,
+						range:{
+							start: {
+								line: diffLine-1,
 								character: declarationName[3] - charOffset
 							},
 							end: {
-								line: diffLine - 1,
+								line: diffLine-1,
 								character: declarationName[3] + word[0].length - charOffset
-
 							}
 						}
 					}
 				}
-			});
-		} 
+			})
+		}
 	})
 	clearTempAST();
 	return finalDefinition;
 }
 
-function clearTempAST() {
+function clearTempAST(){
 	foundDeclaration = [];
 	_foundDeclarationCount = 0;
 }
-
