09429a4e2732f583d8ae29c779934d508693fa94
diyaayay <diya.solanki.31@gmail.com>
2024-09-25 15:35:33 +0530

    modified dep file
    Signed-off-by: diyaayay <diya.solanki.31@gmail.com>

--- dependency.bat
+++ dependency.bat
@@ -2,7 +2,6 @@
 set "zipUrl=https://github.com/diyaayay/processing-language-server-extension/releases/download/v1.0/dependency.zip"
 set "zipFile=jre\dependency.zip"
 set "extractDir=jre\deps"
-mkdir "%extractDir%"
 powershell -Command "Invoke-WebRequest -Uri '%zipUrl%' -OutFile '%cd%\%zipFile%'"
 powershell -Command "Expand-Archive -Path '%cd%\%zipFile%' -DestinationPath '%cd%\%extractDir%'"
 del "%zipFile%"
