80e13ed678c8eb464fcccd9b2136fbbd543c25ba
diyaayay <diya.solanki.31@gmail.com>
2024-10-07 15:55:20 +0530

    Sets sketch directory names to be dynamic

--- client/src/sketchRunner.ts
+++ client/src/sketchRunner.ts
@@ -86,7 +86,19 @@ export class SketchRunner implements vscode.Disposable {
             }
     
             return new Promise((resolve, reject) => {
-                const sketchPath =  path.resolve(__dirname, "..", "..", "SKECHERS");
+                const editor = vscode.window.activeTextEditor;
+                if (!editor) {
+                    // this.logError('No active editor found. Please open a sketch file.');
+                    return false; 
+                }
+                const currentFilePath = editor.document.uri.fsPath; 
+                const sketchDirectory = path.dirname(currentFilePath);
+
+                const sketchPath =  path.resolve(sketchDirectory);
+                
+
+                
+                    
                 const processingJavaPath = path.join(this._jrePath, "processing-sketch.exe");
                 this._child = spawn(processingJavaPath, ["--sketch=" + sketchPath, "--run"], { cwd: this._workDir });
     
