e3af76b0aed91fdd349fef1bee68e5c322f71d6d
Diya Solanki <diya.solanki.31@gmail.com>
2024-10-11 02:13:21 +0530

    fixed deps

--- dependency.bat
+++ dependency.bat
@@ -1,34 +1,7 @@
 @echo off
-set "zipUrl=https://github.com/diyaayay/processing-language-server-extension/releases/download/v1.0/dependency.zip"
+set "zipUrl=https://github.com/diyaayay/processing-language-server-extension/releases/download/v1.2/dependency.zip"
 set "zipFile=jre\dependency.zip"
 set "extractDir=jre\deps"
 powershell -Command "Invoke-WebRequest -Uri '%zipUrl%' -OutFile '%cd%\%zipFile%'"
 powershell -Command "Expand-Archive -Path '%cd%\%zipFile%' -DestinationPath '%cd%\%extractDir%'"
 del "%zipFile%"
-
-
-//Cross platform script
-
-@echo off
-
-:: Define variables for URLs and paths
-set "zipUrl=https://github.com/diyaayay/processing-language-server-extension/releases/download/v1.0/dependency.zip"
-set "zipFile=jre\dependency.zip"
-set "extractDir=jre\deps"
-
-:: Check if running on Windows
-ver | findstr /i "Windows" > nul
-if %errorlevel% == 0 (
-    :: Windows: use PowerShell to download and extract
-    powershell -Command "Invoke-WebRequest -Uri '%zipUrl%' -OutFile '%cd%\%zipFile%'"
-    powershell -Command "Expand-Archive -Path '%cd%\%zipFile%' -DestinationPath '%cd%\%extractDir%'"
-    del "%zipFile%"
-) else (
-    :: Linux: use wget and unzip
-    wget "%zipUrl%" -O "%zipFile%"
-    mkdir -p "%extractDir%"
-    unzip "%zipFile%" -d "%extractDir%"
-    rm "%zipFile%"
-)
-
-echo Script execution completed.
